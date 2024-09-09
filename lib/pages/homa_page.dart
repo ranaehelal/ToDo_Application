309ce180299967e38cb7221aca1abc7154f41b0b
Rana Helal <res.maika@gmail.com>
2024-09-09 22:15:14 +0300

    update the home

--- lib/pages/homa_page.dart
+++ lib/pages/homa_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import '../models/task.dart';
 import '../widgets/task_item.dart';
 
-
 class HomaPage extends StatefulWidget {
   const HomaPage({super.key});
 
@@ -15,16 +14,19 @@ class _HomaPageState extends State<HomaPage> {
     Task(name: "Watch the Tutorial", isCompleted: true),
     Task(name: "Do the exercise", isCompleted: false),
   ];
+
   void _addNewTask() {
     setState(() {
       tasks.add(Task(name: "New Task ${tasks.length + 1}", isCompleted: false));
     });
   }
+
   void _removeTask(int index) {
     setState(() {
       tasks.removeAt(index);
     });
   }
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -48,23 +50,45 @@ class _HomaPageState extends State<HomaPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(25.0),
-        child: ListView(
-          children: tasks.map((task) {
-            return Padding(
-              padding: const EdgeInsets.only(bottom: 25.0),
-              child: TaskItem(
-                taskName: task.name,
-                isCompleted: task.isCompleted,
-                onChanged: (value) {
-                  setState(() {
-                    task.isCompleted = value!;
-                  });
-                },
-                activeColor: theme.colorScheme.primary, // Pass the color
+        child: ListView.builder(
+          itemCount: tasks.length,
+          itemBuilder: (BuildContext context, int index) {
+            final task = tasks[index];
 
-              ),
+            return Column(
+              children: [
+                Dismissible(
+                  key: Key(task.name),
+                  direction: DismissDirection.endToStart, // Swipe right to left
+                  onDismissed: (direction) {
+                    _removeTask(index);
+
+                  },
+                  background: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    alignment: Alignment.centerRight, // Align icon to the right
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Icon(Icons.delete, color: Colors.white), // Delete icon
+                  ),
+
+                  child: TaskItem(
+                    taskName: task.name,
+                    isCompleted: task.isCompleted,
+                    onChanged: (value) {
+                      setState(() {
+                        task.isCompleted = value!;
+                      });
+                    },
+                    activeColor: theme.colorScheme.primary,
+                  ),
+                ),
+                const SizedBox(height: 10.0),
+              ],
             );
-          }).toList(),
+          },
         ),
       ),
     );
